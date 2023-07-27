629e03b72c66ed28c07941f04b80657e86c36696
Victory <v.josephgeo@alustudent.com>
2023-07-27 15:17:41 -0700

    task 4

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,18 +1,8 @@
 #!/usr/bin/python3
-"""function that returns an object (Python data structure)
-represented by a JSON string"""
+"""Defines a JSON-to-object function."""
 import json
 
 
 def from_json_string(my_str):
-    """
-    Return an object by a JSON representation.
-
-    Args:
-        my_str: JSON representation
-
-    Raises:
-        Exception: when the string can't be decoded
-    """
-
-    return json.loads(my_str
+    """Return the Python object representation of a JSON string."""
+    return json.loads(my_str)
